165d8bdf691cb6d2b40231ef03efccb890c45f5b
Antonio Aguilar <jaguilar992@gmail.com>
2018-08-11 22:54:47 -0600

    UI

--- app/cpu.py
+++ app/cpu.py
@@ -2,6 +2,7 @@ import subprocess as sp
 import os
 import re
 
+# Gestion de nucleos del procesador
 class Procesador():
   def __init__(self):
     self.nucleos = []
@@ -25,9 +26,40 @@ class Procesador():
           sp.call(self.apagar % nucleo, shell=True)
   
   def apaga_nucleo (self, n):
-    if (n<self.cuenta_nucleos and n!=0):
-      return sp.call(self.apagar % n, shell=True)
+    if (n<self.numero_nucleos and n!=0):
+      return sp.call(self.apagar % ("cpu"+str(n)), shell=True)
+    else:
+      return None
+  
+  def esta_apagado(self, n):
+    n = int(n)
+    if (n==0):
+      return False
+    else:
+      if (n<self.numero_nucleos):
+        f = open('/sys/devices/system/cpu/cpu%s/online' % n, 'r')
+        c = f.readlines()
+        f.close()
+        print c[0][:-1]==1
+        return c[0][:-1]==1
+      else:
+        return True
+
+  def get_lista_apagados(self):
+    apagados = []
+    for nucleo in self.nucleos:
+      print nucleo[3:]
+      if (self.esta_apagado(nucleo[3:])):
+        apagados.append(nucleo)
+    return apagados
+  
+  def get_lista_encendidos(self):
+    apagados = self.get_lista_apagados()
+
+        
+      
 
 if __name__ == '__main__':
   p = Procesador()
   p.apaga_todos()
+  print p.get_lista_apagados()
